02ff861a8fc6cefd28f53ea92d6666c5e222dab3
brunolcarli <brunolcarli@gmail.com>
2020-12-23 12:24:37 -0500

    ADD api url constant

--- ark/settings.py
+++ ark/settings.py
@@ -4,3 +4,5 @@ from decouple import config
 __version__ = '0.0.0'
 
 TOKEN = config('TOKEN')
+
+DRAGON_IMG = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/COTS2Dragon.6.jpg/800px-COTS2Dragon.6.jpg'
