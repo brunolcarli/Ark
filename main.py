b5b08666e0d371a7fa182986a4a8ca7d77f1f635
Bruno L. Carli <brunolcarli@gmail.com>
2021-02-17 22:15:58 -0300

    ADD replit configs

--- main.py
+++ main.py
@@ -1,22 +1,54 @@
 import logging
 import sys
-from ark.settings import TOKEN, __version__
+from ark.settings import TOKEN, __version__, ENV_REF
 from core.commands import client
+from core.keep_alive import keep_alive
 
 logging.basicConfig(level='INFO')
 log = logging.getLogger()
 
+
 if __name__ == '__main__':
     log.info(
         r'''
+       ^
+      / \
+     /___\
+    |=   =|
+    |     |
+    |     |
+    |     |
+    |     |
+    |     |
+    |     |
+    |     |
+    |     |
+    |     |
+   /|##!##|\
+  / |##!##| \
+ /  |##!##|  \
+|  / ^ | ^ \  |
+| /  ( | )  \ |
+|/   ( | )   \|
+    ((   ))
+   ((  :  ))
+   ((  :  ))
+    ((   ))
+     (( ))
+      ( )
+       .
+       .
+       .
      _    ____  _  __
     / \  |  _ \| |/ /
    / _ \ | |_) | ' / 
   / ___ \|  _ <| . \ 
- /_/   \_\_| \_\_|\_\      
-                    
+ /_/   \_\_| \_\_|\_\                          
         '''
     )
     log.info('Running ARK version: %s\n', __version__)
+
+    if ENV_REF == 'replit':
+        keep_alive()
 
     client.run(TOKEN)
